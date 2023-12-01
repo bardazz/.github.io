bd41de4cb45df84cc9e57fba0692b9ce3957d1dc
bardazz <93321500+bardazz@users.noreply.github.com>
2023-12-01 03:29:27 +0200

    Update laba_5_Gerasimenko.py

--- laba_5_Gerasimenko.py
+++ laba_5_Gerasimenko.py
@@ -11,17 +11,18 @@ def bubble_sort(arr):
         for j in range(0, n - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-bubble_sort(input())
-print(f'Відсортований список: {bubble_sort}')
-#3
+    return arr
+
+user_input = input('Введіть числа, розділені пробілами: ')
+user_list = list(map(int, user_input.split()))  
+sorted_list = bubble_sort(user_list.copy()) 
+print(f'Відсортований список: {sorted_list}')
 my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
-my_list = set(my_list)
-print(f'The list with unique elements only: {my_list}')
-#4
+my_set = set(my_list)  
+print(f'Список з унікальними елементами: {my_set}')
 chess_board = [['_' for _ in range(8)] for _ in range(8)]
-chess_board[0][0] = 'T'
-chess_board[0][8 - 1] = 'T'
-chess_board[8 - 1][0] = 'T'
-chess_board[8 - 1][8 - 1] = 'T'
+positions = [(0, 0), (0, 7), (7, 0), (7, 7)] 
+for row, col in positions:#Peremena
+    chess_board[row][col] = 'T'#PozT
 for row in chess_board:
     print(' '.join(row))

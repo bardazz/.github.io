d46279240ee83208bd0563e3f0d6f052aa063c99
bardazz <93321500+bardazz@users.noreply.github.com>
2023-12-01 03:35:38 +0200

    Update laba_9_Gerasimenko.py

--- laba_9_Gerasimenko.py
+++ laba_9_Gerasimenko.py
@@ -1,30 +1,28 @@
-text = input("Enter your message: ")
 def enter_number():
-    try:
-        num = int(input('Enter a number (1-25): '))
-        if num > 25 or num < 1:
-            raise ValueError
-    except:
-        print('Please enter a number between 1 and 25!')
-        enter_number()
+    text = input("Enter your message: ")
+    def cipher_text():
+        try:
+            num = int(input('Enter a number (1-25): '))
+            if num > 25 or num < 1:
+                raise ValueError
+        except:
+            print('Please enter a number between 1 and 25!')
+            return cipher_text()
+        cipher = ''
+        for char in text:
+            if not char.isalpha():
+                cipher += char
+            else:
+                x_char = char.upper()
+                code = ord(x_char) + num
 
-    cipher = ''
+                if code > 90:
+                    code = 64 + (code - 90)
 
-    for char in text:
-
-        if not char.isalpha():
-            cipher += char
-        else:
-            x_char = char.upper()
-            code = ord(x_char) + num
-
-            if code > 90:
-                code = 64 + (code - 90)
-
-            if char.isupper():
-                cipher += chr(code).upper()
-            elif char.islower():
-                cipher += chr(code).lower()
-
-    print(f'Cipher: {cipher}')
+                if char.isupper():
+                    cipher += chr(code).upper()
+                elif char.islower():
+                    cipher += chr(code).lower()
+        print(f'Cipher: {cipher}')
+    cipher_text()
 enter_number()
